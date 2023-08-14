95a9787d165ff6233d9de90c979f5e29713f71fb
William <109118303+Ww1ll@users.noreply.github.com>
2023-08-13 21:40:42 -0300

    Feat: Cap 1 and 2

---
+++ restAssured/src/test/java/Chapter02Test.java
@@ -0,0 +1,84 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class Chapter02Test {
+
+    @Test
+    public void requestUsZipCode90210_checkStatusCode_expectHttp200(){
+
+        given()
+        .when()
+                .get("http://zippopotam.us/us/90210")
+        .then()
+                .assertThat()
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void requestUsZipCode90210_checkContentTyoe_expectedApplicationJson(){
+        given()
+        .when()
+                .get("http://zippopotam.us/us/90210")
+        .then()
+                .assertThat()
+                .contentType(ContentType.JSON)
+        ;
+    }
+
+    @Test
+    public void requestUsZipCode90210_logRequestAndResponseDetails(){
+        given()
+        .when()
+                .get("http://zippopotam.us/us/90210")
+        .then()
+                .log().body()
+        ;
+    }
+    @Test
+    public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills(){
+        given()
+        .when()
+                .get("http://zippopotam.us/us/90210")
+        .then()
+                .assertThat()
+                .body("places[0].'place name'", equalTo("Beverly Hills"))
+        ;
+    }
+
+    @Test
+    public void requestUsZipCode90210_checkStateNameInResponseBody_expectCalifornia(){
+        given()
+        .when()
+                .get("http://zippopotam.us/us/90210")
+        .then()
+                .assertThat()
+                .body("places[0].state", equalTo("California"))
+        ;
+    }
+    @Test
+    public void requestUsZipCode90210_checkListOfPlaceNameInResponseBody_expectContainsBeverlyHills(){
+        given()
+        .when()
+                .get("http://zippopotam.us/us/90210")
+        .then()
+                .assertThat()
+                .body("places.'place name'", hasItem("Beverly Hills"))
+        ;
+    }
+    @Test
+    public void requestUsZipCode90210_checkNumberOfPlaceNamesInResponseBody_expectOne(){
+        given()
+        .when()
+                .get("http://zippopotam.us/us/90210")
+        .then()
+                .assertThat()
+                // verifica o tamanho de uma coleção(exemplo: lista)
+                .body("places.'place name'", hasSize(1))
+        ;
+    }
+
+}
