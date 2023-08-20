faa3ae4c0bece37e09c424126c741b9732256cc4
William <109118303+Ww1ll@users.noreply.github.com>
2023-08-20 19:28:09 -0300

    feat: add chapter06

--- restAssured/src/test/java/dataEntities/Location.java
+++ restAssured/src/test/java/dataEntities/Location.java
@@ -1,4 +1,4 @@
-package dataEntitites;
+package dataEntities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
--- restAssured/src/test/java/dataEntities/Place.java
+++ restAssured/src/test/java/dataEntities/Place.java
@@ -1,4 +1,4 @@
-package dataEntitites;
+package dataEntities;
 
 import com.fasterxml.jackson.annotation.*;
 
